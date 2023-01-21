ac7626ff57abebeef615775c10a8906130fc4b31
SJRUK2000 <you@example.com>
2023-01-20 23:49:57 -0500

    create post front end

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-import {Card, Loader, FormField} from '../components/index';
+import {Card, Loader, FormField} from '../components';
 
 const RenderCards = ({data, title}) => {
     if(data?.length > 0) {
