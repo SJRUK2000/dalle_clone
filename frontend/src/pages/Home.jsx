d134b3083631a18c304ed47bb1e6978f087f9c11
SJRUK2000 <you@example.com>
2023-01-22 15:35:54 -0500

    backend done not deployed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,6 +16,46 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState('');
+    const [searchTimeout, setSearchTimeout] = useState(null);
+    const [searchedResults, setSearchedResults] = useState(null);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/post', {
+                    method:'GET',
+                    headers : {
+                      "Content-Type":"application/json",
+                    },
+                  })
+
+                  if(response.ok) {
+                    const result = await response.json();
+
+                    setAllPosts(result.data.reverse());
+                  }
+            } catch (error) {
+                alert(error);
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchPosts();
+    }, []);
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
+        setSearchText(e.target.value);
+    
+        setSearchTimeout(
+          setTimeout(() => {
+            const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+            setSearchedResults(searchResult);
+          }, 500),
+        );
+      };
 
   return (
     <section className='m-w-7xl mx-auto'>
@@ -29,7 +69,14 @@ const Home = () => {
         </div>
 
         <div className='mt-16'>
-            <FormField/>
+            <FormField
+            labelName="Search posts"
+            type="text"
+            name="text"
+            placeholder="Search something..."
+            value={searchText}
+            handleChange={handleSearchChange}
+            />
         </div>
 
         <div className='mt-10'>
@@ -47,12 +94,12 @@ const Home = () => {
                 <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
                     {searchText ? (
                         <RenderCards 
-                            data = {[]}
+                            data = {searchedResults}
                             title = "No search results found "
                         />
                     ) : (
                         <RenderCards
-                        data = {[]}
+                        data = {allPosts}
                         title = "No posts found"
                         />
                     )}
